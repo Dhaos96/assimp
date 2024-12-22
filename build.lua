cfdd0eec694403e057df7f817198c18510c96221
Jan Niedens <j.niedens96@gmail.com>
2024-12-22 12:04:12 +0100

    premake file added

---
+++ build.lua
@@ -0,0 +1,36 @@
+project "assimp"
+   kind "StaticLib"
+   language "C++"
+   cppdialect "C++23"
+   staticruntime "off"
+
+   files {"**.h", "**.c", "**.hpp", "**.cpp"}
+
+   includedirs
+   {
+      "include"
+   }
+
+   targetdir ("Binaries/" .. OutputDir .. "/%{prj.name}")
+   objdir ("Binaries/Intermediates/" .. OutputDir .. "/%{prj.name}")
+
+   filter "system:windows"
+       systemversion "latest"
+       defines { }
+
+   filter "configurations:Debug"
+       defines { "DEBUG" }
+       runtime "Debug"
+       symbols "On"
+
+   filter "configurations:Release"
+       defines { "RELEASE" }
+       runtime "Release"
+       optimize "On"
+       symbols "On"
+
+   filter "configurations:Dist"
+       defines { "DIST" }
+       runtime "Release"
+       optimize "On"
+       symbols "Off"
